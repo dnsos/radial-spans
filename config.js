a756a2f58393db375b2b25440764b1116a785a65
Dennis <dennisostendorf@web.de>
2018-11-10 22:23:01 +0200

    Added update function, new styling etc.

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-console.log('Hello radial spans!');
-
-const width = 960,
+const width = 750,
     height = 500,
     barHeight = height / 2,
-    innerCircleRadius = 20;
+    innerCircleRadius = 40;
 
 const color = d3.scaleOrdinal(["#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f"]);
 
@@ -14,5 +12,72 @@ const svg = d3.select('#viz').append("svg")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 d3.json("data/data.json", function (error, data) {
-    createGraph(data[0].values);
+
+    let selector = d3.select("#selector__variable")
+        .on("change", onChange);
+
+    selector
+        .selectAll("option")
+        .data(data)
+        .enter().append("option")
+        .text((d) => { return d.variable; });
+
+    createGraph(data[0]);
+
+    function onChange() {
+        let selectedValue = d3.select("#selector__variable").property("value")
+
+        let selectedIndex = data.findIndex(d => d.variable == selectedValue);
+
+        updateData(data[selectedIndex]);
+    }
 });
+
+function updateData(data) {
+
+    d3.select("#control__variable").text(data.variable);
+    d3.select("#control__route").text(data.route);
+    d3.select("#control__unit").text(data.unit);
+    d3.select("#control__respondants").text(data.values.length);
+
+    const values = data.values;
+
+    let extent = d3.extent(values.flat());
+
+    let barScale = d3.scaleLinear()
+        .domain(extent)
+        .range([innerCircleRadius, barHeight]);
+
+    let x = d3.scaleLinear()
+        .domain(extent)
+        .range([-innerCircleRadius, -barHeight]);
+
+    let xAxis = d3.axisLeft(x)
+        .ticks(5);
+
+    svg.selectAll(".circle__axis")
+        .data(x.ticks(5))
+        .transition()
+        .duration(700)
+        .attr("r", function (d) { return barScale(d); })
+
+    d3.selectAll(".line__distance")
+        .data(data.values)
+        .transition()
+        .duration(700)
+        .attr("y1", -innerCircleRadius)
+        .attr("y2", function (d, i) {
+            return barScale(-d[1]) - innerCircleRadius * 2;
+        });
+
+    d3.selectAll(".line__span")
+        .data(data.values)
+        .transition()
+        .duration(700)
+        .attr("y1", function (d, i) {
+            return barScale(-d[0]) - innerCircleRadius * 2;
+        })
+        .attr("y2", function (d, i) {
+            return barScale(-d[1]) - innerCircleRadius * 2;
+        });
+}
--- graph.js
+++ graph.js
@@ -1,20 +1,19 @@
 function createGraph(data) {
 
-    console.log(data);
+    d3.select("#control__variable").text(data.variable);
+    d3.select("#control__route").text(data.route);
+    d3.select("#control__unit").text(data.unit);
+    d3.select("#control__respondants").text(data.values.length);
 
-    let extent = d3.extent(data, function (d, i) { return d[i]; });
-    console.log(extent);
+    const values = data.values;
 
-    extent = [0,50];
-
-    console.log(extent);
-    
+    let extent = d3.extent(values.flat());
     
     let barScale = d3.scaleLinear()
         .domain(extent)
         .range([innerCircleRadius, barHeight]);
 
-    let numBars = data.length;
+    let numBars = values.length;
 
     let x = d3.scaleLinear()
         .domain(extent)
@@ -23,25 +22,48 @@ function createGraph(data) {
     let xAxis = d3.axisLeft(x)
         .ticks(5);
 
-    let circles = svg.selectAll("circle")
+    // BACKGROUND CIRCLES
+    let circles = svg.append("g")
+        .classed("circles__wrapper", true);
+
+    svg.select(".circles__wrapper")
+        .append("circle")
+        .attr("r", barHeight)
+        .style("fill", "white");
+
+    svg.select(".circles__wrapper")
+        .selectAll("circle")
         .data(x.ticks(5))
         .enter().append("circle")
+        .classed("circle__axis", true)
         .attr("r", function (d) { return barScale(d); })
         .style("fill", "none")
-        .style("stroke", "white")
+        .style("stroke", "#dddddd")
         .style("stroke-dasharray", "5,5")
         .style("stroke-width", "1px");
 
-    svg.append("circle")
-        .attr("r", barHeight)
-        .classed("outer", true)
-        .style("fill", "none")
-        .style("stroke", "white")
-        .style("stroke-width", "1.5px");
+    // LINES
+    let linesWrapper = svg.selectAll("lines__wrapper")
+        .data(values)
+        .enter().append("g")
+        .classed("lines__wrapper", true)
+        .attr("transform", function (d, i) { return "rotate(" + (i * 360 / numBars) + ")"; });
 
-    let lines = svg.selectAll("line")
-        .data(data)
-        .enter().append("line")
+    let lineDistance = linesWrapper
+        .datum((d) => { return d })
+        .append("line")
+        .classed("line__distance", true)
+        .attr("y1", -innerCircleRadius)
+        .attr("y2", function (d, i) { 
+            return barScale(-d[1]) - innerCircleRadius * 2; 
+        })
+        .style("stroke", "#a3a3a3")
+        .style("stroke-width", "1px");
+
+    let lineSpan = linesWrapper
+        .datum((d) => { return d })
+        .append("line")
+        .classed("line__span", true)
         .attr("y1", function (d, i) { 
             return barScale(-d[0]) - innerCircleRadius * 2; 
         } )
@@ -49,22 +71,40 @@ function createGraph(data) {
             return barScale(-d[1]) - innerCircleRadius * 2; 
         })
         .style("stroke", "#dddddd")
-        .style("stroke-width", "6px")
-        .attr("transform", function (d, i) { return "rotate(" + (i * 360 / numBars) + ")"; })
+        .style("stroke-width", "10px")
         .on("mouseover", function (d) { 
-            d3.select(this).attr("y1", -innerCircleRadius);
-            d3.select(this).style("stroke", "#a3a3a3"); 
+            d3.select(this)
+                .transition()
+                .duration(200)
+                //.attr("y1", -innerCircleRadius)
+                .style("stroke", "#b81214"); 
         })
         .on("mouseout", function (d) { 
-            d3.select(this).attr("y1", barScale(-d[0]) - innerCircleRadius * 2);
-            d3.select(this).style("stroke", "#dddddd"); 
+            d3.select(this)
+                .transition()
+                .duration(200)
+                //.attr("y1", barScale(-d[0]) - innerCircleRadius * 2)
+                .style("stroke", "#dddddd"); 
         });
 
     svg.append("g")
         .attr("class", "x axis")
         .call(xAxis);
 
-    // Labels
+    // DESTINATION
+    let circleDestination = svg.append("circle")
+        .classed("circle__destination", true)
+        .attr("r", innerCircleRadius)
+        .style("fill", "white")
+        .style("stroke", "none");
+
+    svg.append("image")
+        .attr("xlink:href","assets/shenkar_logo.svg")
+        .attr("width", innerCircleRadius)
+        .attr("height", innerCircleRadius)
+        .attr("transform", "translate(" + (-innerCircleRadius / 2) + "," + (-innerCircleRadius / 2) + ")");
+
+    // LABELS
     let labelRadius = barHeight * 1.025;
 
     let labels = svg.append("g")
